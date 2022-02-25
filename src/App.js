048cb8c7b421ca305d5ab332488d36e22a522418
Michael Callaro <michael.callaro@hcahealthcare.com>
2022-02-25 13:03:01 -0600

    Added react-bootstrap and uuid to the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Button, Stack } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+
 function App() {
   return (
-    <h1>Hi Callaro!</h1>
+    <Container className="my-4">
+      <Stack className="mb-4" direction="horizontal" gap="2">
+        <h1 className='me-auto'>Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '1rem', alignItems: 'flex-start'}}></div>
+    </Container>
   );
 }
 
